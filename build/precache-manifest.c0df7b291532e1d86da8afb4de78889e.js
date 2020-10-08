a491fdf3aa068d5d2d60b603323da83a6df27b1d
IgorVerhusha <viarkhusha93@gmail.com>
2020-10-08 20:40:26 +0300

    lesson 95 пробую деплоить

---
+++ build/precache-manifest.c0df7b291532e1d86da8afb4de78889e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9601a2b52cffaee1fc2b2425834b8c72",
+    "url": "/mySocialNetwork/index.html"
+  },
+  {
+    "revision": "4f74c9b5bd6a5107bfaf",
+    "url": "/mySocialNetwork/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "d0d3687d20b3eca383b5",
+    "url": "/mySocialNetwork/static/css/main.623ca59a.chunk.css"
+  },
+  {
+    "revision": "4f74c9b5bd6a5107bfaf",
+    "url": "/mySocialNetwork/static/js/2.22072f81.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mySocialNetwork/static/js/2.22072f81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c433af566c332447e5b",
+    "url": "/mySocialNetwork/static/js/3.92f31d83.chunk.js"
+  },
+  {
+    "revision": "d0d3687d20b3eca383b5",
+    "url": "/mySocialNetwork/static/js/main.5b7c72d4.chunk.js"
+  },
+  {
+    "revision": "311348587e551a7a6684",
+    "url": "/mySocialNetwork/static/js/runtime-main.70e9b927.js"
+  }
+]);
